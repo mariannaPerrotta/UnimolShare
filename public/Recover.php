0c86cc47a17aa72d9b0ae2835d51106df3597d9c
Dani2097 <danilo.sprovieri9@gmail.com>
2018-05-12 19:22:03 +0200

    prova

--- public/Recover.php
+++ public/Recover.php
@@ -1,7 +1,15 @@
-<?php
+
 /**
  * Created by PhpStorm.
  * User: Danilo
- * Date: 12/05/2018
+ 12/05/2018
  * Time: 18:53
  */
+<html>
+    <head>
+        <title>Password dimenticata?</title>
+        <link rel="stylesheet" type="text/css" href="styleIndex.css" />
+        <script src="http://code.jquery.com/"
+
+    </head>
+<body>
