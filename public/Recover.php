f9e7c117299697fbcf0dc13e3247c7ec0b9f6731
Franciu <franciuran@gmail.com>
2018-05-14 22:36:39 +0200

    Ho aggiunto la funzione Modifica profilo che aggiorna i dati nome, cognome e password di un profilo

--- public/Recover.php
+++ public/Recover.php
@@ -1,15 +1,27 @@
+<?php
+// endpoint: /registration
+$app->post('/registration', function (Request $request, Response $response) {
+$db = new DBQueryManager();
 
-/**
- * Created by PhpStorm.
- * User: Danilo
- 12/05/2018
- * Time: 18:53
- */
-<html>
-    <head>
-        <title>Password dimenticata?</title>
-        <link rel="stylesheet" type="text/css" href="styleIndex.css" />
-        <script src="http://code.jquery.com/"
+$requestData = $request->getParsedBody();//Dati richiesti dal servizio REST
+$email = $requestData['email'];
+$password = $requestData['password'];
+$nome = $requestData['nome'];
+$cognome = $requestData['cognome'];
 
-    </head>
-<body>
+//Risposta del servizio REST
+$responseData = array(); //La risposta è un array di informazioni da compilare
+
+//Controllo la risposta dal DB e compilo i campi della risposta
+if (!$db->registration($email)) { //Se l'email non esiste prosegue con la registrazione
+$responseData['error'] = false; //Campo errore = false
+    //to do funzione di popolamento del database
+$responseData['message'] = 'Registrazione'; //Messaggio di esito positivo
+
+} else { //Se l'email già è stata registrata in precedenza
+$responseData['error'] = true; //Campo errore = true
+$responseData['message'] = 'Email associata a un account già esistente!'; //Messaggio di esito negativo
+}
+return $response->withJson($responseData); //Invio la risposta del servizio REST al client
+});
+?>
