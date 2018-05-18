ad86f1c62d809a517c3a76b633fabf9ff0bd73e9
MariannaPerrotta <mariannaperrotta95@gmail.com>
2018-05-18 17:37:39 +0200

    commit

--- public/index.php
+++ public/index.php
@@ -32,17 +32,17 @@ $settings = require __DIR__ . '/../src/settings.php';
 $app = new App($settings); //"Contenitore" per gli endpoint da riempire
 
 
-//$app->options('/{routes:.+}', function ($request, $response, $args) {
-//    return $response;
-//});
-//
-//$app->add(function ($req, $res, $next) {
-//    $response = $next($req, $res);
-//    return $response
-//        ->withHeader('Access-Control-Allow-Origin', '*')
-//        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-//        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-//});
+$app->options('/{routes:.+}', function ($request, $response, $args) {
+    return $response;
+});
+
+$app->add(function ($req, $res, $next) {
+    $response = $next($req, $res);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', '*')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+});
 
 
 /*  Gli endpoint sono delle richieste http accessibili al Client gestite poi dal nostro Server REST.
